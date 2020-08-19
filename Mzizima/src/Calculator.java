f440f67d75a0bdac2b135c0590ede6644fbba4e7
Peter Ndeji <Peter Ndeji@192.168.137.181>
2020-08-20 02:22:48 +0300

    Najaribu tena nione kama itakubali

---
+++ Mzizima/src/Calculator.java
@@ -0,0 +1,11 @@
+	import java.io.*;
+public class Calculator {
+
+	public static void main(String[] args) {
+		String s1 = getInput("Enter a numerical value");
+		String s1 = getInput("Enter a numerical value");
+		
+		double d1 = Double.
+	}
+
+}
